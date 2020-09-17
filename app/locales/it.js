a1be3c40e036775eef39bf3a4f9428ccc9cf8b99
Andrea Racco <andrea.racco@sixeleven.it>
2020-09-17 12:43:22 +0200

    Create it.js

---
+++ app/locales/it.js
@@ -0,0 +1,3 @@
+export let itDays = ["DOM", "LUN", "MAR", "MER", "GIO", "VEN", "SAB"];
+export let itMonths = ["GENNAIO", "FEBBRAIO", "MARZO", "APRILE", "MAGGIO", "GIUGNO", "LUGLIO", "AGOSTO", "SETTEMBRE", "OTTOBRE", "NOVEMBRE", "DICEMBRE"];
+export let itMonthsShort = ["GEN", "FEB", "MAR", "APR", "MAG", "GIU", "LUG", "AGO", "SET", "OTT", "NOV", "DIC"];
